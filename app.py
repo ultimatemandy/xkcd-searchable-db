079448dec840a47fe408e8beaee470029df8f27b
Amanda Zoe de Moura Theodorakoglou <135218134+ultimatemandy@users.noreply.github.com>
2024-11-26 13:11:30 +0100

    new

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def test_connection():
         return jsonify({"error": f"Failed to connect to MongoDB: {str(e)}"})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
