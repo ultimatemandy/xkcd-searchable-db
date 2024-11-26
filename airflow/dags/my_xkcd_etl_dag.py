5b5382ce0453a5743aec54a5526fc596798f277d
Amanda Zoe de Moura Theodorakoglou <135218134+ultimatemandy@users.noreply.github.com>
2024-11-26 14:45:59 +0100

    new

--- airflow/dags/my_xkcd_etl_dag.py
+++ airflow/dags/my_xkcd_etl_dag.py
@@ -2,7 +2,7 @@ import json
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
-import requests
+from fetch_data import fetch_xkcd_data, clean_and_optimize_data, export_to_mongodb
 from hdfs import InsecureClient
 from pymongo import MongoClient
 
@@ -22,55 +22,6 @@ def clear_mongo_collection():
     collection = db['comics']
     result = collection.delete_many({})
     print(f"Cleared {result.deleted_count} documents from the MongoDB collection.")
-
-# Function to fetch data from XKCD and save it to HDFS
-def fetch_data():
-    url = 'https://xkcd.com/info.0.json'
-    response = requests.get(url)
-    data = response.json()
-    print("Fetched data from XKCD API:")
-    print(json.dumps(data, indent=2))
-
-    hdfs_client = InsecureClient('http://localhost:9870', user='hdfs')
-    with hdfs_client.write('/user/hdfs/xkcd_data.json', encoding='utf-8') as writer:
-        json.dump(data, writer)
-
-# Function to clean and process the raw data
-def clean_data():
-    hdfs_client = InsecureClient('http://localhost:9870', user='hdfs')
-    with hdfs_client.read('/user/hdfs/xkcd_data.json', encoding='utf-8') as reader:
-        data = json.load(reader)
-
-    print("Raw data read from HDFS:")
-    print(json.dumps(data, indent=2))
-
-    cleaned_data = {
-        'num': data['num'],
-        'title': data['title'],
-        'img': data['img'],
-        'alt': data['alt']
-    }
-
-    print("Cleaned data:")
-    print(json.dumps(cleaned_data, indent=2))
-
-    with hdfs_client.write('/user/hdfs/xkcd_cleaned_data.json', encoding='utf-8') as writer:
-        json.dump(cleaned_data, writer)
-
-# Function to export the cleaned data to MongoDB
-def export_to_mongo():
-    client = MongoClient('mongodb://localhost:27017/')
-    db = client['xkcd']
-    collection = db['comics']
-
-    hdfs_client = InsecureClient('http://localhost:9870', user='hdfs')
-    with hdfs_client.read('/user/hdfs/xkcd_cleaned_data.json', encoding='utf-8') as reader:
-        cleaned_data = json.load(reader)
-
-    print("Cleaned data to be exported to MongoDB:")
-    print(json.dumps(cleaned_data, indent=2))
-
-    collection.insert_one(cleaned_data)
 
 # Define the DAG
 dag = DAG(
@@ -96,19 +47,19 @@ clear_mongo_task = PythonOperator(
 
 fetch_task = PythonOperator(
     task_id='fetch_data',
-    python_callable=fetch_data,
+    python_callable=fetch_xkcd_data,
     dag=dag
 )
 
 clean_task = PythonOperator(
     task_id='clean_data',
-    python_callable=clean_data,
+    python_callable=clean_and_optimize_data,
     dag=dag
 )
 
 export_task = PythonOperator(
     task_id='export_to_mongo',
-    python_callable=export_to_mongo,
+    python_callable=export_to_mongodb,
     dag=dag
 )
 
